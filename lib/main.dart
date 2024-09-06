ba5735a420a7ede427fc58ebd51da55d30405675
Dzaki Arrozaq <dzakiarrozaq62@students.unnes.ac.id>
2024-09-06 12:56:48 +0700

    Stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() =>runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: const Text('My First App', style: TextStyle(color: Colors.white)),
-      centerTitle: true,
-      backgroundColor: Colors.blue[900],
-    ),
-    body: Center(
-      child: Text(
+  home: Home(),
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My First App', style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Center(
+        child: Text(
           'Hallo Dzaki',
           style: TextStyle(
             fontSize: 20.0,
@@ -17,13 +23,15 @@ void main() =>runApp(MaterialApp(
             color: Colors.grey[600],
             fontFamily: 'Poppins',
           ),
+        ),
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () { null; },
-      backgroundColor: Colors.blue[900],
-      child: const Text('click', style: TextStyle(color: Colors.white),),
-    ),
-  ),
-));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () { null; },
+        backgroundColor: Colors.blue[900],
+        child: const Text('click', style: TextStyle(color: Colors.white),),
+      ),
+    );
+  }
+}
+
 
