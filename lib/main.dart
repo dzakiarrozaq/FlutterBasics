7037359c54572e4357fe3b3e724396a2ebb2005e
Dzaki Arrozaq <dzakiarrozaq62@students.unnes.ac.id>
2024-09-06 11:43:53 +0700

    Lesson 3

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,26 @@ import 'package:flutter/material.dart';
 void main() =>runApp(MaterialApp(
   home: Scaffold(
     appBar: AppBar(
-      title: const Text('My First App'),
+      title: const Text('My First App', style: TextStyle(color: Colors.white)),
       centerTitle: true,
+      backgroundColor: Colors.blue[900],
     ),
-    body: const Center(
-      child: Text('Hallo Dzaki'),
+    body: Center(
+      child: Text(
+          'Hallo Dzaki',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'Poppins',
+          ),
+      ),
     ),
     floatingActionButton: FloatingActionButton(
       onPressed: () { null; },
-      child: const Text('click'),
+      backgroundColor: Colors.blue[900],
+      child: const Text('click', style: TextStyle(color: Colors.white),),
     ),
   ),
 ));
